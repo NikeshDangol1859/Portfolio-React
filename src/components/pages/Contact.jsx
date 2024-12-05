bcafe490bfb81caff1def4a8a70c5746e38bdf66
NikeshDangol1859 <nikeshdangol126@gmail.com>
2024-12-05 11:46:04 +0545

    comi

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -11,7 +11,7 @@ function Contact() {
                     <i className="fa-solid fa-phone" />
                 </div>
                 <h2 className="contact-heading">Contact Number</h2>
-                <p className="contact-detail">9861476935 / 9818621859</p>
+                <p className="contact-detail"> <a href="tel:+9779861476935">9861476935</a> / <a href="tel:+9779818621859">9818621859</a></p>
                 </div>
                 <div className="contact-card">
                 <div className="contact-icon">
@@ -30,7 +30,7 @@ function Contact() {
             </div>           
             <div className="contact-grid justify-content-center mt-5 ">
                 <div className="contanct-footer ">
-                <h2 className=" find text-white mb-4 ">Also  <span className="text-warning text-decoration-underline">Find Me on: </span> </h2>
+                <h2 className=" find text-white mb-4 ">Also  <span className="findme text-decoration-underline">Find Me on: </span> <span className='animation-findme'></span> </h2>
                 <div className="icons d-flex ">
                     <a href="https://www.instagram.com/neekesh_dangol/" className="icons-contact" target="_blank" style={{marginRight: 30}} data-toggle="tooltip" data-placement="bottom" title="instagram"><i className="fa-brands fa-instagram" /></a>
                     <a href="https://www.facebook.com/profile.php?id=100007808718971" className="icons-contact" target="_blank" style={{marginRight: 30}} data-toggle="tooltip" data-placement="bottom" title="facebook"><i className="fa-brands fa-facebook" /></a>
